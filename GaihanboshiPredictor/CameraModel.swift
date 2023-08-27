5892fe85ed7364c2d1ece482540afd5d320ce573
taki-ser <rt12201311@gmail.com>
2023-08-27 18:25:10 +0900

    カメラMV分解

--- GaihanboshiPredictor/CameraModel.swift
+++ GaihanboshiPredictor/CameraModel.swift
@@ -5,4 +5,89 @@
 //  Created by 滝瀬隆斗 on 2023/08/27.
 //
 
-import Foundation
+import AVFoundation
+import UIKit
+class CameraModel: ObservableObject{
+    //    カメラセッションをclassプロパティとして定義
+    let captureSession = AVCaptureSession()
+    
+    //    Delegateのインスタンス生成
+    private var photoCaptureDelegate = PhotoCaptureDelegate()
+    
+    //    撮影関数
+    func takePicture(flashMode: Bool) {
+        guard let photoOutput = captureSession.outputs.first as? AVCapturePhotoOutput else { return }
+        let photoSettings = AVCapturePhotoSettings()
+        photoSettings.flashMode = .off
+
+        #if targetEnvironment(simulator)
+        #else
+        guard let device = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera,
+            for: AVMediaType.video, // ビデオ入力
+            position: AVCaptureDevice.Position.back)
+        else{ return }
+        if device.hasFlash {
+            if device.isFlashAvailable {
+                if flashMode == true {
+                    photoSettings.flashMode = .on
+                }
+            }
+        }
+        #endif
+        
+        
+        photoOutput.capturePhoto(with: photoSettings, delegate: photoCaptureDelegate)
+    }
+    //    撮影関数で使用するDelegate
+    class PhotoCaptureDelegate: NSObject, AVCapturePhotoCaptureDelegate, ObservableObject {
+        @Published var imageForPreview: UIImage?
+        init(imageForPreview: UIImage? = nil) {
+            self.imageForPreview = imageForPreview
+        }
+        // 写真撮影後の処理を実装する
+        func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+            // 写真が取得されたら、ここで処理を行う
+            if let imageData = photo.fileDataRepresentation(), let image = UIImage(data: imageData){
+                print(image)
+                imageForPreview = image
+            }
+        }
+    }
+    //    カメラ初期設定
+    func setupCamera() {
+        //    シミュレータ上ではセッション設定をスキップ
+        #if targetEnvironment(simulator)
+        #else
+        captureSession.beginConfiguration()
+        connectInputsToSession()
+        connectOutputToSession()
+        captureSession.commitConfiguration()
+        DispatchQueue.global(qos: .background).async {
+            self.captureSession.startRunning()
+        }
+        #endif
+        //    Input設定
+        func connectInputsToSession() {
+            let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .unspecified)
+            guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!), captureSession.canAddInput(videoDeviceInput)
+            else {
+                print("error. Input設定エラー")
+                return
+                
+            }
+            captureSession.addInput(videoDeviceInput)
+            
+        }
+        //    Output設定
+        func connectOutputToSession() {
+            let photoOutput = AVCapturePhotoOutput()
+            guard captureSession.canAddOutput(photoOutput)
+            else {
+                print("error. Output設定エラー")
+                return
+            }
+            captureSession.sessionPreset = .photo
+            captureSession.addOutput(photoOutput)
+        }
+    }
+}
--- GaihanboshiPredictor/CameraView.swift
+++ GaihanboshiPredictor/CameraView.swift
@@ -9,19 +9,17 @@ import SwiftUI
 import AVFoundation
 //    Viewの定義
 struct CameraView: View {
-    //    カメラセッションをclassプロパティとして定義
-    let captureSession = AVCaptureSession()
+    //    CameraModelインスタンスを生成
+    private let cameraModel = CameraModel()
     @State var flashMode = false
-    //    Delegateのインスタンス生成
-    @StateObject private var photoCaptureDelegate = PhotoCaptureDelegate()
     //    撮影された写真
     @State var previewImage: UIImage?
     //    body定義
     var body: some View {
         VStack {
             Spacer()
-            PreviewViewUIView(captureSession: captureSession)
-                .onAppear(perform: {setupCamera()})
+            PreviewViewUIView(captureSession: cameraModel.captureSession)
+                .onAppear(perform: {cameraModel.setupCamera()})
                 .frame(width: UIScreen.main.bounds.width)
                 .frame(height: UIScreen.main.bounds.width/3*4)
                 .background(Color.white)
@@ -50,7 +48,7 @@ struct CameraView: View {
                         .stroke(Color.white, lineWidth:5)
                         .frame(width: 90, height:90)
                     Button(action: {
-                        takePicture(flashMode: flashMode)
+                        cameraModel.takePicture(flashMode: flashMode)
                     }) {
                         Circle()
                             .fill(Color.white)
@@ -67,84 +65,6 @@ struct CameraView: View {
         .background(Color.gray)
     }
     
-    //    撮影関数
-    func takePicture(flashMode: Bool) {
-        guard let photoOutput = captureSession.outputs.first as? AVCapturePhotoOutput else { return }
-        let photoSettings = AVCapturePhotoSettings()
-        photoSettings.flashMode = .off
-
-        #if targetEnvironment(simulator)
-        #else
-        guard let device = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera,
-            for: AVMediaType.video, // ビデオ入力
-            position: AVCaptureDevice.Position.back)
-        else{ return }
-        if device.hasFlash {
-            if device.isFlashAvailable {
-                if flashMode == true {
-                    photoSettings.flashMode = .on
-                }
-            }
-        }
-        #endif
-        
-        
-        photoOutput.capturePhoto(with: photoSettings, delegate: photoCaptureDelegate)
-    }
-    //    撮影関数で使用するDelegate
-    class PhotoCaptureDelegate: NSObject, AVCapturePhotoCaptureDelegate, ObservableObject {
-        @Published var imageForPreview: UIImage?
-        init(imageForPreview: UIImage? = nil) {
-            self.imageForPreview = imageForPreview
-        }
-        // 写真撮影後の処理を実装する
-        func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-            // 写真が取得されたら、ここで処理を行う
-            if let imageData = photo.fileDataRepresentation(), let image = UIImage(data: imageData){
-                print(image)
-                imageForPreview = image
-            }
-        }
-    }
-    //    カメラ初期設定
-    private func setupCamera() {
-        //    シミュレータ上ではセッション設定をスキップ
-        #if targetEnvironment(simulator)
-        #else
-        captureSession.beginConfiguration()
-        connectInputsToSession()
-        connectOutputToSession()
-        captureSession.commitConfiguration()
-        DispatchQueue.global(qos: .background).async {
-            self.captureSession.startRunning()
-        }
-        #endif
-        //    Input設定
-        func connectInputsToSession() {
-            let videoDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .unspecified)
-            guard let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!), captureSession.canAddInput(videoDeviceInput)
-            else {
-                print("error")
-                return
-                
-            }
-            captureSession.addInput(videoDeviceInput)
-            
-        }
-        //    Output設定
-        func connectOutputToSession() {
-            let photoOutput = AVCapturePhotoOutput()
-            guard captureSession.canAddOutput(photoOutput)
-            else {
-                print("error")
-                return
-                
-            }
-            captureSession.sessionPreset = .photo
-            captureSession.addOutput(photoOutput)
-        }
-    }
-  
     //    preview用画面UIView
     class PreviewView: UIView {
         override class var layerClass: AnyClass {
